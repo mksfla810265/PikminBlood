f65d91da829e0fe820d695809f42b4b491f38e39
James.Xu <James.Xu@newtype.com.tw>
2022-04-27 22:03:04 +0800

    feat: 新增圖片點擊變換效果

--- js/index.js
+++ js/index.js
@@ -1,22 +1,41 @@
 const { createApp, reactive, toRefs } = Vue;
 const app = {
-   setup() {
-      const state = reactive({
-         PikminArr: [
-            { img: "Red", isHave: false },
-            { img: "Yellow", isHave: false },
-            { img: "Blue", isHave: false },
-            { img: "White", isHave: false },
-            { img: "Purple", isHave: false },
-            { img: "Pink", isHave: false },
-            { img: "Gray", isHave: false }
-         ]
-      });
+	setup() {
+		const state = reactive({
+			Restaurant: [
+				{ img: "Red", isHave: false },
+				{ img: "Yellow", isHave: false },
+				{ img: "Blue", isHave: false },
+				{ img: "White", isHave: false },
+				{ img: "Purple", isHave: false },
+				{ img: "Pink", isHave: false },
+				{ img: "Gray", isHave: false }
+			],
+			Cafe: [
+				{ img: "Red", isHave: false },
+				{ img: "Yellow", isHave: false },
+				{ img: "Blue", isHave: false },
+				{ img: "White", isHave: false },
+				{ img: "Purple", isHave: false },
+				{ img: "Pink", isHave: false },
+				{ img: "Gray", isHave: false }
+			]
+		});
 
-      return {
-         ...toRefs(state)
-      }
-   }
+		const changeImgColor = (Location, index) => {
+			if (Location === "Restaurant") {
+				state.Restaurant[index].isHave = !state.Restaurant[index].isHave;
+			}
+			else if (Location === "Cafe") {
+				state.Cafe[index].isHave = !state.Cafe[index].isHave;
+			}
+		}
+
+		return {
+			...toRefs(state),
+			changeImgColor
+		}
+	}
 };
 
 createApp(app).mount("#app");
