0b6d96737b8ed5f352c5e32e01cc7140c5105d0b
James.Xu <James.Xu@newtype.com.tw>
2022-04-27 21:49:51 +0800

    test

--- js/index.js
+++ js/index.js
@@ -2,7 +2,15 @@ const { createApp, reactive, toRefs } = Vue;
 const app = {
    setup() {
       const state = reactive({
-         
+         PikminArr: [
+            { img: "Red", isHave: false },
+            { img: "Yellow", isHave: false },
+            { img: "Blue", isHave: false },
+            { img: "White", isHave: false },
+            { img: "Purple", isHave: false },
+            { img: "Pink", isHave: false },
+            { img: "Gray", isHave: false }
+         ]
       });
 
       return {
