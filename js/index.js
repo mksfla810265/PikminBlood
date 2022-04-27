fae98c0a33652247373f5e7b3b3f9f48c748ed8d
James.Xu <James.Xu@newtype.com.tw>
2022-04-27 23:52:31 +0800

    feat: 新增total 數量計算

--- js/index.js
+++ js/index.js
@@ -2,33 +2,42 @@ const { createApp, reactive, toRefs } = Vue;
 const app = {
 	setup() {
 		const state = reactive({
-			Restaurant: [
-				{ img: "Red", isHave: false },
-				{ img: "Yellow", isHave: false },
-				{ img: "Blue", isHave: false },
-				{ img: "White", isHave: false },
-				{ img: "Purple", isHave: false },
-				{ img: "Pink", isHave: false },
-				{ img: "Gray", isHave: false }
-			],
-			Cafe: [
-				{ img: "Red", isHave: false },
-				{ img: "Yellow", isHave: false },
-				{ img: "Blue", isHave: false },
-				{ img: "White", isHave: false },
-				{ img: "Purple", isHave: false },
-				{ img: "Pink", isHave: false },
-				{ img: "Gray", isHave: false }
-			]
+			Restaurant: {
+				Pikmins: [
+					{ img: "Red", noHave: true },
+					{ img: "Yellow", noHave: true },
+					{ img: "Blue", noHave: true },
+					{ img: "White", noHave: true },
+					{ img: "Purple", noHave: true },
+					{ img: "Pink", noHave: true },
+					{ img: "Gray", noHave: true }
+				],
+				total: 0
+			},
+			Cafe: {
+				Pikmins: [
+					{ img: "Red", noHave: true },
+					{ img: "Yellow", noHave: true },
+					{ img: "Blue", noHave: true },
+					{ img: "White", noHave: true },
+					{ img: "Purple", noHave: true },
+					{ img: "Pink", noHave: true },
+					{ img: "Gray", noHave: true }
+				],
+				total: 0
+			}
 		});
 
 		const changeImgColor = (Location, index) => {
-			if (Location === "Restaurant") {
-				state.Restaurant[index].isHave = !state.Restaurant[index].isHave;
+			state[Location].Pikmins[index].noHave = !state[Location].Pikmins[index].noHave;
+
+			let count = 0;
+			for (const item of state[Location].Pikmins) {
+				if (item.noHave === false) {
+					count++;
+				}
 			}
-			else if (Location === "Cafe") {
-				state.Cafe[index].isHave = !state.Cafe[index].isHave;
-			}
+			state[Location].total = count;
 		}
 
 		return {
