2b0f4ea5a733723af3a227de64e47763e599384c
James.Xu <James.Xu@newtype.com.tw>
2022-04-28 23:39:58 +0800

    feat: 新增資料儲存localStorage

--- js/index.js
+++ js/index.js
@@ -2,35 +2,26 @@ const { createApp, reactive, toRefs } = Vue;
 const app = {
 	setup() {
 		const state = reactive({
-			Restaurant: {
-				Pikmins: [
-					{ img: "Red", noHave: true },
-					{ img: "Yellow", noHave: true },
-					{ img: "Blue", noHave: true },
-					{ img: "White", noHave: true },
-					{ img: "Purple", noHave: true },
-					{ img: "Pink", noHave: true },
-					{ img: "Gray", noHave: true }
-				],
-				total: 0
-			},
-			Cafe: {
-				Pikmins: [
-					{ img: "Red", noHave: true },
-					{ img: "Yellow", noHave: true },
-					{ img: "Blue", noHave: true },
-					{ img: "White", noHave: true },
-					{ img: "Purple", noHave: true },
-					{ img: "Pink", noHave: true },
-					{ img: "Gray", noHave: true }
-				],
-				total: 0
-			}
+			Data: {}
 		});
 
-		const changeImgColor = (Location, index) => {
-			state[Location].Pikmins[index].noHave = !state[Location].Pikmins[index].noHave;
-			state[Location].total = state[Location].Pikmins.filter(e => !e.noHave).length;
+		const changeImgColor = (User, i, j) => {
+			state.Data[User][i].Pikmins[j].noHave = !state.Data[User][i].Pikmins[j].noHave;
+			state.Data[User][i].total = state.Data[User][i].Pikmins.filter(e => !e.noHave).length;
+
+			window.localStorage.setItem("Pikmins", JSON.stringify(state.Data));
+		}
+
+		if (window.localStorage.getItem("Pikmins") === null) {
+			fetch("./data/init.json")
+				.then(res => res.json())
+				.then(data => {
+					//console.log("Data", data);
+					state.Data = data;
+				});
+		}
+		else {
+			state.Data = JSON.parse(window.localStorage.getItem("Pikmins"));
 		}
 
 		return {
